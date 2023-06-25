10c9293104bc1df899b1ca4696f0084d7550124c
Brayan Durán Medina <brayan.duran@leaguesofcode.com>
2023-06-25 03:05:09 +0200

    store file added

--- storage_server.py
+++ storage_server.py
@@ -1,4 +1,4 @@
-from flask import Flask, Blueprint
+from flask import Flask, Blueprint, request
 import os
 
 app = Flask(__name__)
@@ -9,6 +9,32 @@ apiv1 = Blueprint("v1", __name__, url_prefix="/v1")
 @apiv1.route("/ping", methods=["GET"])
 def hello_world():
     return "server " + os.environ.get("SERVER_ID") + " response: pong"
+
+
+@apiv1.route("/store", methods=["POST"])
+def store_file():
+    if (
+        not request.json
+        or not "filename" in request.json
+        or not "content" in request.json
+    ):
+        return "filename or content not provided", 400
+
+    filename = request.json["filename"]
+    content = request.json["content"]
+
+    storage_path = "storage/server" + os.environ.get("SERVER_ID")
+
+    if os.path.exists(storage_path + "/" + filename):
+        return "file already exists", 400
+
+    if not os.path.exists(storage_path):
+        os.makedirs(storage_path)
+
+    with open(storage_path + "/" + filename, "w") as f:
+        f.write(content)
+
+    return "file stored", 200
 
 
 app.register_blueprint(apiv1)
